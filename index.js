e1100d2c6debe0eca0952f938e9c471c52ef8d66
David Bruant <bruant.d@gmail.com>
2017-12-03 18:26:12 +0100

    Mise en place du dépôt

---
+++ index.js
@@ -0,0 +1,5 @@
+export default function(doc){
+    throw `TODO`
+
+    return doc;
+}
---
+++ test/index.js
@@ -0,0 +1,10 @@
+import {expect} from 'chai';
+
+import anonymize from '../index.js';
+
+describe('anonymize', () => {
+    it('should be a function', () => {
+        expect(anonymize).to.be.a('function');
+    })
+});
+  
